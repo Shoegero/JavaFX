662f9669172fe5ad42b41a2edaba8c9eb213c880
Shoegero <razial99@bk.ru>
2023-03-14 05:32:55 +0300

    Revert "TelegramBot"
    This reverts commit f42bc79cea782e9794798b123e8ece96dab635e3.

--- java/TelegramBot/service/database/DatabaseService.java
+++
@@ -1,8 +0,0 @@
-package TelegramBot.service.database;
-
-import lombok.SneakyThrows;
-
-public interface DatabaseService {
-    @SneakyThrows
-    void add(String name, String score, String status, int episodes, String release, String url);
-}
